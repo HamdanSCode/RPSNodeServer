ccd875b90386b0e5d9d6a72221418a5ef75b576e
HamdanSCode <hamdan.mobeen@gmail.com>
2023-06-28 10:18:35 -0700

    UPDATED LINT

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express, { response } from 'express';
-const app = express();
-const port = 3000;
 import fs from 'fs/promises';
 
+const app = express();
+const port = 3000;
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello World!' });
+  res.json({ message: 'Hello World!' });
 });
 
 app.post('/', (req, res) => {
-    console.log(req.body.test)
-    res.json({ message: "sup" })
+  console.log(req.body.test);
+  res.json({ message: 'sup' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port} xd dx`);
 });
